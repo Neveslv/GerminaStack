38576a33583764f784bd3cfea9e838c91f0d1bb9
Nicolas25vlad <nicolas25vlad@proton.me>
2026-08-06 10:31:03 -0300

    feat: abrir perfis públicos

--- routes/account.go
+++ routes/account.go
@@ -8,6 +8,7 @@ import (
 
 func RegisterAccountRoutes(api *gin.RouterGroup, account *handlers.AccountHandler, authenticated gin.HandlerFunc) {
 	routes := api.Group("", authenticated)
+	routes.GET("/users/:username", account.GetPublicProfile)
 	routes.GET("/me", account.GetProfile)
 	routes.PATCH("/me", account.UpdateProfile)
 	routes.GET("/me/preferences", account.GetPreferences)
